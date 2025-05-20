5138e10f012baab1d766e7f162fc2cc3b4c5ba31
Andres Guerra <andresguerra0625@gmail.com>
2025-05-19 20:55:09 -0500

    changes

--- sections/Benchmarking.jsx
+++ sections/Benchmarking.jsx
@@ -15,25 +15,28 @@ export default function Benchmarking() {
           className="rounded-lg object-contain"
           priority
         />
-        <motion.h2
-          initial={{ opacity: 0, x: -40 }}
-          whileInView={{ opacity: 1, x: 0 }}
-          transition={{ duration: 0.8 }}
-          viewport={{ once: true }}
-          className="text-2xl sm:text-3xl font-semibold mb-6 tracking-wide"
-        >
-          Benchmarking
-        </motion.h2>
-        <motion.p
-          initial={{ opacity: 0, x: 40 }}
-          whileInView={{ opacity: 1, x: 0 }}
-          transition={{ duration: 1, delay: 0.2 }}
-          viewport={{ once: true }}
-          className="text-lg sm:text-xl font-light md:text-left max-w-2xl"
-        >
-          Realizando el Benchmarking y analizar el mercado competitivo logramos sentirnos cautivadas por <b className="font-bold">Chanel.</b><br /><br />
-          Al entrar en la web pudimos conectar con la marca y vivir la experiencia que ofrece. Tiene una sección grande de historia y una gran interactividad que incluso te permite probarte las gafas de manera virtual. Además, la información e imágenes se presentan de forma atractiva, lo que permite que el usuario navegue las distintas secciones que tiene.
-        </motion.p>
+        <div>
+
+          <motion.h2
+            initial={{ opacity: 0, x: -40 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.8 }}
+            viewport={{ once: true }}
+            className="text-2xl sm:text-3xl font-semibold mb-6 tracking-wide"
+          >
+            Benchmarking
+          </motion.h2>
+          <motion.p
+            initial={{ opacity: 0, x: 40 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            transition={{ duration: 1, delay: 0.2 }}
+            viewport={{ once: true }}
+            className="text-lg sm:text-xl font-light md:text-left max-w-2xl"
+          >
+            Realizando el Benchmarking y analizar el mercado competitivo logramos sentirnos cautivadas por <b className="font-bold">Chanel.</b><br /><br />
+            Al entrar en la web pudimos conectar con la marca y vivir la experiencia que ofrece. Tiene una sección grande de historia y una gran interactividad que incluso te permite probarte las gafas de manera virtual. Además, la información e imágenes se presentan de forma atractiva, lo que permite que el usuario navegue las distintas secciones que tiene.
+          </motion.p>
+        </div>
       </div>
       <Image
         src="/navbar.png"
@@ -45,6 +48,14 @@ export default function Benchmarking() {
       />
       <Image
         src="/podcast.png"
+        alt="navbar"
+        width={800}
+        height={400}
+        className="block max-w-4xl w-full h-auto object-contain rounded-lg mt-8 mx-auto"
+        priority
+      />
+      <Image
+        src="/buyer.png"
         alt="navbar"
         width={800}
         height={400}
